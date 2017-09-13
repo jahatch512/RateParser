8102dc8e6373cfb857473e95a399f19a06ab2539
jhatch <jhatch@shopathome.com>
2017-09-12 21:49:49 -0600

    added react/webpack config setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/javascripts/root.jsx",
+  output: {
+    path: path.join(__dirname, '/src/javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        },
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx" ]
+  }
+};
